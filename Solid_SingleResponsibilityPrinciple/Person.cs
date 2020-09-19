75cdd04c4d80dde56cd2e5f05f7a0a49ff69d531
shuheydev <shuhey.dev@gmail.com>
2020-09-19 21:13:53 +0900

    Add project files.

---
+++ Solid_SingleResponsibilityPrinciple/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Solid_SingleResponsibilityPrinciple
+{
+    internal class Person
+    {
+        public Person()
+        {
+        }
+
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+    }
+}
